fdf0e035ff739a99aafc3d0c4094491ae4a1b73d
Atiyah Ellerbee <atiyahbellerbee@gmail.com>
2021-06-30 17:58:58 -0400

    getting a lot of this uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Sitebar from "./home/Navbar";
+import Auth from "./auth/Auth";
+import WorkoutIndex from "./workouts/WorkoutIndex";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App () {
+  const [sessionToken, setSessionToken] = useState('')
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'))
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken)
+    setSessionToken(newToken)
+    console.log(sessionToken)
+  }
+
+  const clearToken = () => {
+    localStorage.clear()
+    setSessionToken('')
+  }
+
+  const protectedViews = () => {
+    return (sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken}/>
+     : <Auth updateToken={updateToken}/>)
+  }
+
+  return(
+    <div>
+      <Sitebar clearToken={clearToken} />
+      {protectedViews()}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/workouts/WorkoutIndex.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import WorkoutCreate from './WorkoutCreate'
+import WorkoutTable from './WorkoutTable'
+
+const WorkoutIndex = (props) => {
+    const [workouts, setWorkouts] = useState([])
+
+    const fetchWorkouts = () => {
+        fetch('http://localhost:3000/log', {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${props.token}`
+            })
+        }).then((res) => res.json())
+            .then((logData) => {
+                setWorkouts(logData)
+            })
+    }
+
+    useEffect(() => {
+        fetchWorkouts()
+    }, [])
+
+    return(
+        <Container>
+            <Row>
+                <Col md="3">
+                    <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token}/>
+                </Col>
+                <Col md="9">
+                    <WorkoutTable workouts={workouts} fetchWorkouts={fetchWorkouts} token={props.token}/>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default WorkoutIndex
